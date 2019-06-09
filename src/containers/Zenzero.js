60f35317175a783a7c5f7996db9530b40be35ebf
Krrish Mittal <takyyon@Krrishs-MacBook-Pro.local>
2019-06-09 10:58:40 -0400

    zenzero container setup

---
+++ src/containers/Zenzero.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Zenzero extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                Zenzero Container
+            </div>
+        );
+    }
+}
+
+export default Zenzero;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Zenzero from './containers/Zenzero';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <div className='container-fluid wbdv-root-container'>
+        <Zenzero />
+    </div>,
+    document.getElementById('root')
+ )
