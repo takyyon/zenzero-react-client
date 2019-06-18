97d6f20222720b7053f08bb92940b77eb3d7b09c
Krrish Mittal <takyyon@Krrishs-MacBook-Pro.local>
2019-06-18 16:14:29 -0400

    search input text added

--- src/containers/Zenzero.js
+++ src/containers/Zenzero.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './../components/Home';
+import './index.scss';
 
 class Zenzero extends Component {
     constructor(props){
@@ -12,12 +13,42 @@ class Zenzero extends Component {
 
     render() {
         return (
-            <Router>
-                <Route exact path='/' 
-                    render={() => 
-                        <Home />
-                    }/>
-            </Router>
+            <div className='zenzero-container'>
+                <div className='row zenzero-header'>
+                    <div className='col-3'>
+
+                    </div>
+                    <div className='col-6'>
+                        <div className="input-group mb-3 zenzero-search">
+                            <input type="text" className="form-control" placeholder="Search Restaurant" aria-label="Search Restaurant" aria-describedby="basic-addon2" />
+                            <div className="input-group-append">
+                                <button className="btn btn-outline-secondary" type="button">
+                                    <i className="fas fa-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-3'>
+                        <span className='dropdown'>
+                            <a className='dropdown-toggle'>
+                                <i className="fas fa-user-circle"></i>
+                            </a>
+                        </span>
+                        
+                    </div>
+                </div>
+                <div className='row zenzero-body'>
+                    <Router>
+                        <Route exact path='/' 
+                            render={() => 
+                                <Home />
+                            }/>
+                    </Router>
+                </div>
+                <div className='row zenzero-footer'>
+
+                </div>
+            </div>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import Zenzero from './containers/Zenzero';
 
 ReactDOM.render(
-    <div className='container-fluid wbdv-root-container'>
+    <div className='container-fluid' id='zenzero'>
         <Zenzero />
     </div>,
     document.getElementById('root')
