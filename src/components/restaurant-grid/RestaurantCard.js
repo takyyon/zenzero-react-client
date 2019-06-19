bf3fa8ec432e7b97d348f46772292d5ef4f8074f
Krrish Mittal <takyyon@Krrishs-MacBook-Pro.local>
2019-06-18 20:04:43 -0400

    restaurant card setup

--- src/components/restaurant-grid/RestaurantCard.js
+++ src/components/restaurant-grid/RestaurantCard.js
@@ -3,8 +3,23 @@ import './index.scss';
 
 const RestaurantCard = ({ restaurant }) => {
     return (
-        <div>
-
+        <div className='col-lg-3 col-md-4 col-sm-12'>
+            <div className='card'>
+                <img className='card-img-top'
+                    alt='Thumbnail' />
+                <div className='card-body'>
+                    <p className='card-title'>
+                        {restaurant.name}
+                    </p>
+                    <p className='card-text'>
+                        <i className='fas fa-file-alt'></i>
+                        
+                    </p>
+                </div>
+                <div className='card-footer'>
+                    
+                </div>
+            </div>
         </div>
     );
 };
